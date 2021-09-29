b2d018ab8d653f9c6f437e4d147d011a9ada1a55
eduadmin <eduadmin@PL7-Instructor-C02X71LZJ1G6.local>
2021-09-29 11:31:13 -0400

    1st topsec commit

--- myassets/js/alum.js
+++ myassets/js/alum.js
@@ -3,3 +3,23 @@ console.log('POGGERS');
 
 //initialize AOS.js
 AOS.init();
+
+//jquery initialization
+$(function(){
+
+$('#scrollbtn1').on('click', function(e){
+  e.preventDefault();
+  $('html, body').animate({scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+});
+
+
+
+
+
+
+
+
+
+
+
+});
